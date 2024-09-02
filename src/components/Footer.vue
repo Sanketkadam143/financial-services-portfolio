fda76475fc5bcdd1c1b880facbb7b4530cf061b5
sanket <sanketanilkadam@gmail.com>
2024-09-02 18:07:51 +0530

    added visitor count

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,56 +7,19 @@
             Let's keep in touch!
           </h4>
           <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-            Copyright {{ currentYear }} ©  All rights reserved. | Designed & Developed by HSPM
-            Solutions LLP.
+            Copyright {{ currentYear }} © All rights reserved. | Designed &
+            Developed by HSPM Solutions LLP.
           </h5>
-          <!-- component -->
-          <div class="flex mx-auto">
-            <div class="container mx-auto">
-              <div>
-                <div class="flex flex-wrap gap-2"></div>
-              </div>
-            </div>
+
+          <!-- Display the visitor count image -->
+          <div class="flex mx-auto mt-4">
+            <img
+              src="https://dhfservices.in/api/admin/images/counter"
+              alt="Website Visitors"
+              class="w-48"
+            />
           </div>
         </div>
-        <!-- <div class="w-full lg:w-6/12 px-4 my-10 md:my-0">
-        <div class="flex flex-wrap items-top mb-6">
-          <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div> -->
       </div>
       <hr class="my-6 border-blueGray-300" />
       <div
@@ -74,8 +37,8 @@
 export default {
   data() {
     return {
-      currentYear: new Date().getFullYear()
+      currentYear: new Date().getFullYear(),
     };
-  }
+  },
 };
 </script>
